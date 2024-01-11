d474365aeb10a76d6cb0471757c378ab5b677d2c
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2024-01-11 01:17:20 -0300

    tests(parser): Re-add some tests

--- src/parser.rs
+++ src/parser.rs
@@ -179,79 +179,166 @@ impl Parser {
     }
 }
 
-#[test]
-fn parse_level() -> Result<(), ParserError> {
-    assert_eq!(level("V")?, Level::Verbose);
-    assert_eq!(level("D")?, Level::Debug);
-    assert_eq!(level("I")?, Level::Info);
-    assert_eq!(level("W")?, Level::Warn);
-    assert_eq!(level("E")?, Level::Error);
-    assert_eq!(level("F")?, Level::Fatal);
-    assert_eq!(level("A")?, Level::Assert);
-    assert!(level("INEXISTENT").is_err());
-    Ok(())
-}
+#[cfg(test)]
+mod test {
+    use super::{
+        level, printable, CsvParser, DefaultParser, FormatParser, JsonParser, Parser, ParserError,
+    };
+    use crate::record::Level;
 
-#[test]
-fn parse_printable() {
-    let t = "03-01 02:19:45.207     1     2 I EXT4-fs (mmcblk3p8): mounted filesystem with \
+    #[test]
+    fn parse_level() -> Result<(), ParserError> {
+        assert_eq!(level("V")?, Level::Verbose);
+        assert_eq!(level("D")?, Level::Debug);
+        assert_eq!(level("I")?, Level::Info);
+        assert_eq!(level("W")?, Level::Warn);
+        assert_eq!(level("E")?, Level::Error);
+        assert_eq!(level("F")?, Level::Fatal);
+        assert_eq!(level("A")?, Level::Assert);
+        assert!(level("INEXISTENT").is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn parse_printable() -> Result<(), ParserError> {
+        let parser = DefaultParser {};
+        let text = "03-01 02:19:45.207    1     2 I EXT4-fs (mmcblk3p8): mounted filesystem with \
              ordered data mode. Opts: (null)";
-    let p = DefaultParser {};
-    let r = p.try_parse_str(t).unwrap();
-    assert_eq!(r.level, Level::Info);
-    assert_eq!(r.tag, "EXT4-fs (mmcblk3p8)");
-    assert_eq!(r.process, "1");
-    assert_eq!(r.thread, "2");
-    assert_eq!(
-        r.message,
-        "mounted filesystem with ordered data mode. Opts: (null)"
-    );
+        let record = parser.try_parse_str(text)?;
+        assert_eq!(record.time, Some("03-01 02:19:45.207".to_string()));
+        assert_eq!(record.level, Level::Info);
+        assert_eq!(record.tag, "EXT4-fs (mmcblk3p8)");
+        assert_eq!(record.process, "1");
+        assert_eq!(record.thread, "2");
+        assert_eq!(
+            record.message,
+            "mounted filesystem with ordered data mode. Opts: (null)"
+        );
 
-    let t = "03-01 02:19:42.868     0     0 D /soc/aips-bus@02100000/usdhc@0219c000: \
+        let text = "03-01 02:19:42.868     0     0 D /soc/aips-bus@02100000/usdhc@0219c000: \
              voltage-ranges unspecified";
-    let r = p.try_parse_str(t).unwrap();
-    assert_eq!(r.level, Level::Debug);
-    assert_eq!(r.tag, "/soc/aips-bus@02100000/usdhc@0219c000");
-    assert_eq!(r.process, "0");
-    assert_eq!(r.thread, "0");
-    assert_eq!(r.message, "voltage-ranges unspecified");
+        let record = parser.try_parse_str(text)?;
+        assert_eq!(record.level, Level::Debug);
+        assert_eq!(record.tag, "/soc/aips-bus@02100000/usdhc@0219c000");
+        assert_eq!(record.process, "0");
+        assert_eq!(record.thread, "0");
+        assert_eq!(record.message, "voltage-ranges unspecified");
 
-    let t = "11-06 13:58:53.582 31359 31420 I GStreamer+amc: 0:00:00.326067533 0xb8ef2a00";
-    let r = p.try_parse_str(t).unwrap();
-    assert_eq!(r.time, Some("11-06 13:58:53.582".to_string()));
-    assert_eq!(r.level, Level::Info);
-    assert_eq!(r.tag, "GStreamer+amc");
-    assert_eq!(r.process, "31359");
-    assert_eq!(r.thread, "31420");
-    assert_eq!(r.message, "0:00:00.326067533 0xb8ef2a00");
+        let text = "11-06 13:58:53.582 31359 31420 I GStreamer+amc: 0:00:00.326067533 0xb8ef2a00";
+        let record = parser.try_parse_str(text)?;
+        assert_eq!(record.time, Some("11-06 13:58:53.582".to_string()));
+        assert_eq!(record.level, Level::Info);
+        assert_eq!(record.tag, "GStreamer+amc");
+        assert_eq!(record.process, "31359");
+        assert_eq!(record.thread, "31420");
+        assert_eq!(record.message, "0:00:00.326067533 0xb8ef2a00");
 
-    let t = "11-06 13:58:53.582 31359 31420 A GStreamer+amc: 0:00:00.326067533 0xb8ef2a00";
-    let r = p.try_parse_str(t).unwrap();
-    assert_eq!(r.level, Level::Assert);
-    assert_eq!(r.tag, "GStreamer+amc");
-    assert_eq!(r.process, "31359");
-    assert_eq!(r.thread, "31420");
-    assert_eq!(r.message, "0:00:00.326067533 0xb8ef2a00");
+        let text = "03-26 13:17:38.345     0     0 W [114416.534450,0] mdss_dsi_off-: ";
+        let record = parser.try_parse_str(text)?;
+        assert_eq!(record.level, Level::Warn);
+        assert_eq!(record.tag, "[114416.534450,0] mdss_dsi_off-");
+        assert_eq!(record.message, "");
+        Ok(())
+    }
 
-    let t = "03-26 13:17:38.345     0     0 I [114416.534450,0] mdss_dsi_off-: ";
-    let r = p.try_parse_str(t).unwrap();
-    assert_eq!(r.level, Level::Info);
-    assert_eq!(r.tag, "[114416.534450,0] mdss_dsi_off-");
-    assert_eq!(r.message, "");
-}
+    #[test]
+    fn parse_printable_unparseable() {
+        let text = "01-10 23:56:50.101   NOTAPID  NOTATHREAD I \
+            am_create_service: [0,119985987,.PersistentIntentOperationService,10137,1755]";
+        assert!(printable(text).is_err());
 
-#[test]
-fn test_parse_csv() {
-    let t = "07-01 14:13:14.446000000,Sensor:batt_therm:29000 mC,Info,ThermalEngine,225,295,07-01 14:13:14.446   225   295 I ThermalEngine: Sensor:batt_therm:29000 mC";
-    let p = CsvParser {};
-    let r = p.try_parse_str(t).unwrap();
-    assert_eq!(r.level, Level::Info);
-    assert_eq!(r.tag, "ThermalEngine");
-    assert_eq!(r.process, "225");
-    assert_eq!(r.thread, "295");
-    assert_eq!(r.message, "Sensor:batt_therm:29000 mC");
-    assert_eq!(
-        r.raw,
-        "07-01 14:13:14.446   225   295 I ThermalEngine: Sensor:batt_therm:29000 mC"
-    );
+        let text = "I hate the antichrist";
+        assert!(printable(text).is_err());
+
+        assert!(printable("").is_err());
+    }
+
+    #[test]
+    fn parse_csv() {
+        let parser = CsvParser {};
+        let text = "07-01 14:13:14.446,Sensor:batt_therm:29000 mC,Info,ThermalEngine,\
+        225,295,07-01 14:13:14.446   225   295 I ThermalEngine: Sensor:batt_therm:29000 mC";
+        let record = parser.try_parse_str(text).unwrap();
+        assert_eq!(record.time, Some("07-01 14:13:14.446".to_string()));
+        assert_eq!(record.level, Level::Info);
+        assert_eq!(record.tag, "ThermalEngine");
+        assert_eq!(record.process, "225");
+        assert_eq!(record.thread, "295");
+        assert_eq!(record.message, "Sensor:batt_therm:29000 mC");
+        assert_eq!(
+            record.raw,
+            "07-01 14:13:14.446   225   295 I ThermalEngine: Sensor:batt_therm:29000 mC"
+        );
+    }
+
+    #[test]
+    fn parse_csv_unparseable() {
+        let parser = CsvParser {};
+        assert!(parser.try_parse_str("").is_err());
+        assert!(parser.try_parse_str(",,,").is_err());
+    }
+
+    #[test]
+    fn parse_json() {
+        let parser = JsonParser {};
+        let text = r#"{
+            "time":"01-10 23:02:59.028",
+            "message":"ViewPostIme pointer 1",
+            "level":"Debug",
+            "tag":"ViewRootImpl@c365f0e[NovaLauncher]",
+            "process":"6496",
+            "thread":"6496",
+            "raw":"01-10 23:02:59.028  6496  6496 D ViewRootImpl@c365f0e[NovaLauncher]: ViewPostIme pointer 1"
+        }"#;
+        let record = parser.try_parse_str(text).unwrap();
+
+        assert_eq!(record.time, Some("01-10 23:02:59.028".to_string()));
+        assert_eq!(record.process, "6496");
+        assert_eq!(record.thread, "6496");
+        assert_eq!(record.level, Level::Debug);
+        assert_eq!(record.tag, "ViewRootImpl@c365f0e[NovaLauncher]");
+        assert_eq!(record.message, "ViewPostIme pointer 1");
+    }
+
+    // For input coming from files or STDIN
+    #[test]
+    fn parser_detector() {
+        let mut parser = Parser::default();
+        parser.parse(" xd ");
+        // No valid parser = no last parser
+        assert!(parser.last.is_none());
+
+        let csv = "01-11 00:16:39.451,\"Screen is off, stop charging anim\",Debug,\
+            BatteryMeterDrawable,1344,1344,\"01-11 00:16:39.451  1344  1344 D \
+            BatteryMeterDrawable: Screen is off, stop charging anim\"";
+
+        let record = parser.parse(csv);
+        assert!(parser.last.is_some_and(|index| index == 1));
+        assert_eq!(record.tag, "BatteryMeterDrawable");
+
+        let raw =
+            "01-11 01:05:30.308  6408  6408 D tun2socks: INFO(tun2socks): UDP: to DNS 33 bytes";
+        let record = parser.parse(raw);
+        assert!(parser.last.is_some_and(|index| index == 0));
+        assert_eq!(record.message, "INFO(tun2socks): UDP: to DNS 33 bytes");
+
+        let json = r#"{
+            "time":"01-11 01:10:02.755",
+            "message":"processEventsLocked: type=0 Count=2 code=2 value=0 deviceId=1 when=11049299731000",
+            "level":"Info",
+            "tag":"InputReader",
+            "process":"882",
+            "thread":"1203",
+            "raw":"01-11 01:10:02.755   882  1203 I InputReader: processEventsLocked: type=0 Count=2 code=2 value=0 deviceId=1 when=11049299731000"
+        }"#;
+        let record = parser.parse(json);
+        assert!(parser.last.is_some_and(|index| index == 2));
+        assert_eq!(record.process, "882");
+
+        let unparseable = parser.parse("i hate the antichrist");
+        assert!(parser.last.is_some_and(|index| index == 2));
+        assert!(unparseable.tag.is_empty());
+        assert!(unparseable.process.is_empty());
+        assert!(unparseable.time.is_none());
+    }
 }
